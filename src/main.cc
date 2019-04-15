8c50ddb650cb938e9190eb9d70013281ce4a89bb
Kathryn Baldauf <kbaldauf@cs.utexas.edu>
2019-04-15 16:18:20 -0500

    reverted page table latency, attempting new optimal

--- src/main.cc
+++ src/main.cc
@@ -160,9 +160,7 @@ void finish_warmup()
     SCHEDULING_LATENCY = 6;
     EXEC_LATENCY = 1;
     PAGE_TABLE_LATENCY = 100;
-    // PAGE_TABLE_LATENCY = 0;
     SWAP_LATENCY = 100000;
-    // SWAP_LATENCY = 0;
 
     cout << endl;
     for (uint32_t i=0; i<NUM_CPUS; i++) {
