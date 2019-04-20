3794b533fd6f16e34b20057cac83d3b045799e04
KarthikVelayutham <karthik.velayutham@gmail.com>
2019-04-20 18:20:25 -0500

    added some comments to the code

--- prefetcher/delta.h
+++ prefetcher/delta.h
@@ -10,7 +10,7 @@
 typedef struct predicted_distance {
 	uint64_t distance;
 	uint64_t lru;
-	
+
 	predicted_distance(uint64_t d) : distance(d), lru(0){}
 }p_distance;
 
@@ -22,11 +22,11 @@ class Delta {
 	uint64_t previous_distance;
 	uint64_t current_distance;
 	uint64_t current_page;
-	
+
 	private:
 		uint64_t get_page_addr(uint64_t full_addr) {
 			uint64_t page = (full_addr >> LOG2_PAGE_SIZE) << LOG2_PAGE_SIZE;
-			return page;	
+			return page;
 		}
 
 		void set_current_values(uint64_t full_addr) {
@@ -34,7 +34,8 @@ class Delta {
 			current_page = page;
 			current_distance = (page - last_accessed_page);
 		}
-		
+
+        // Helper method to see if the distance already exists in the delta table
 		int find_distance_in_prev() {
 			int size = d_table[previous_distance].size();
 			for (int i = 0; i < size; i++) {
@@ -44,7 +45,8 @@ class Delta {
 			}
 			return -1;
 		}
-		
+
+        // Update all of the LRU values, set current index to be 0 since it is MRU
 		void lru_update(int index) {
 			int max = d_table[previous_distance].size();
 			for (int i = 0; i < max; i++) {
@@ -60,26 +62,26 @@ class Delta {
 					// we've found a value to evict, replace with current
 					d_table[previous_distance][i]->distance = current_distance;
 					d_table[previous_distance][i]->lru = 0;
-					return i;					
+					return i;
 				}
 			}
-			// TODO: return an error code? 
+			// TODO: return an error code?
 			return 0;
-		}	
-		
-		
+		}
+
+
 		void update_prev_predicted_distances() {
 			uint64_t size = d_table[previous_distance].size();
 			int exists_index = find_distance_in_prev();
-			
+
 			if (exists_index != -1) {
 				// distance already present in previous predicted, just update
 				lru_update(exists_index);
 			} else if (size < S) {
 				// array is not full, we can just push
-				p_distance *new_dist = new  p_distance(current_distance); 
+				p_distance *new_dist = new  p_distance(current_distance);
 				d_table[previous_distance].push_back(new_dist);
-				lru_update(size); 
+				lru_update(size);
 			} else {
 				// array is full, find value to evict
 				int index = lru_evict();
@@ -89,11 +91,13 @@ class Delta {
 
 		void update_previous_values() {
 			last_accessed_page = current_page;
-			previous_distance = current_distance; 
+			previous_distance = current_distance;
 			current_page = 0;
 			current_distance = 0;
 		}
 
+        // This method takes in a vector of distances and adds them to the page addresses
+        // so that we can get a vector of addresses with these offsets
 		std::vector<uint64_t> distances_to_addresses(std::vector<p_distance*> &distances) {
 			int size = distances.size();
 			if (size == 0) {
@@ -107,24 +111,24 @@ class Delta {
 		}
 
 	public:
-		Delta(uint64_t slots) : S(slots), 
+		Delta(uint64_t slots) : S(slots),
 			last_accessed_page(0),
 			previous_distance(0),
 			current_distance(0),
 			current_page(0) {}
-		
+
 		~Delta(){}
-	
+
 
 		std::vector<uint64_t> find_prefetch_addrs(uint64_t full_addr) {
 			set_current_values(full_addr);
-			
+
 			std::vector<p_distance*> predicted = d_table[current_distance];
-			
+
 			update_prev_predicted_distances();
-			update_previous_values(); 
-			
-			// turn a vector of structs into a vector of addresses	
+			update_previous_values();
+
+			// turn a vector of structs into a vector of addresses
 			return distances_to_addresses(predicted);
 		}
 };
